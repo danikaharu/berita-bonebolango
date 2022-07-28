391102482e2385f53c255be51fd9bbe70aa97ff3
danikaharu <dani.kaharu17@gmail.com>
2022-07-28 23:00:22 +0800

    Add status field

--- resources/views/users/include/form.blade.php
+++ resources/views/users/include/form.blade.php
@@ -3,8 +3,7 @@
         <div class="form-group">
             <label for="name">{{ __('Name') }}</label>
             <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror"
-                placeholder="{{ __('Name') }}" value="{{ isset($user) ? $user->name : old('name') }}" required
-                autofocus>
+                placeholder="{{ __('Name') }}" value="{{ isset($user) ? $user->name : old('name') }}" autofocus>
             @error('name')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -16,8 +15,9 @@
     <div class="col-md-6">
         <div class="form-group">
             <label for="email">{{ __('Email') }}</label>
-            <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror"
-                placeholder="{{ __('Email') }}" value="{{ isset($user) ? $user->email : old('email') }}" required>
+            <input type="email" name="email" id="email"
+                class="form-control @error('email') is-invalid @enderror" placeholder="{{ __('Email') }}"
+                value="{{ isset($user) ? $user->email : old('email') }}">
             @error('email')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -30,8 +30,7 @@
         <div class="form-group">
             <label for="password">{{ __('Password') }}</label>
             <input type="password" name="password" id="password"
-                class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('Password') }}"
-                {{ empty($user) ? 'required' : '' }}>
+                class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('Password') }}">
             @error('password')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -44,7 +43,7 @@
         <div class="form-group">
             <label for="password-confirmation">{{ __('Password Confirmation') }}</label>
             <input type="password" name="password_confirmation" id="password-confirmation" class="form-control"
-                placeholder="{{ __('Password Confirmation') }}" {{ empty($user) ? 'required' : '' }}>
+                placeholder="{{ __('Password Confirmation') }}">
         </div>
     </div>
 
@@ -69,8 +68,26 @@
 
         <div class="col-md-6">
             <div class="form-group">
+                <label for="status">{{ __('Status') }}</label>
+                <select class="form-select" name="status" id="status" class="form-control">
+                    <option value="" selected disabled>{{ __('-- Select status --') }}</option>
+                    <option value="1">Aktif</option>
+                    <option value="0">Tidak Aktif</option>
+                    @error('status')
+                        <div class="invalid-feedback">
+                            <i class="bx bx-radio-circle"></i>
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="form-group">
                 <label for="avatar">{{ __('Avatar') }}</label>
-                <input type="file" name="avatar" id="avatar" class="form-control @error('avatar') is-invalid @enderror">
+                <input type="file" name="avatar" id="avatar"
+                    class="form-control @error('avatar') is-invalid @enderror">
                 @error('avatar')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -78,6 +95,8 @@
                 @enderror
             </div>
         </div>
+
+
     @endempty
 
     @isset($user)
@@ -86,7 +105,7 @@
                 <div class="form-group">
                     <label for="role">{{ __('Role') }}</label>
                     <select class="form-select" name="role" id="role" class="form-control">
-                        <option value="" selected disabled>{{ __('-- Select role --') }}</option>
+                        <option selected disabled>{{ __('-- Select role --') }}</option>
                         @foreach ($roles as $role)
                             <option value="{{ $role->id }}"
                                 {{ $user->getRoleNames()->toArray() !== [] && $user->getRoleNames()[0] == $role->name ? 'selected' : '-' }}>
@@ -102,7 +121,25 @@
                 </div>
             </div>
 
-            <div class="col-md-1 text-center">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="status">{{ __('Status') }}</label>
+                    <select class="form-select" name="status" id="status"
+                        class="form-control @error('status') is-invalid @enderror">
+                        <option selected disabled>{{ __('-- Select status --') }}</option>
+                        <option value="1" @selected($user->status == 1)>Aktif</option>
+                        <option value="0" @selected($user->status == 0)>Tidak Aktif</option>
+                        @error('status')
+                            <div class="invalid-feedback">
+                                <i class="bx bx-radio-circle"></i>
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-md-2 text-center">
                 <div class="avatar avatar-xl">
                     @if ($user->avatar == null)
                         <img src="https://www.gravatar.com/avatar/{{ md5(strtolower(trim($user->email))) }}&s=500"
@@ -113,10 +150,11 @@
                 </div>
             </div>
 
-            <div class="col-md-5 me-0 pe-0">
+            <div class="col-md-10 me-0 pe-0">
                 <div class="form-group">
                     <label for="avatar">{{ __('Avatar') }}</label>
-                    <input type="file" name="avatar" class="form-control @error('avatar') is-invalid @enderror" id="avatar">
+                    <input type="file" name="avatar" class="form-control @error('avatar') is-invalid @enderror"
+                        id="avatar">
                     @error('avatar')
                         <div class="invalid-feedback">
                             <i class="bx bx-radio-circle"></i>
