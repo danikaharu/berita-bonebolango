e6c7f1f9d5dbab20933c38bae4821cea7f6c8f2c
danikaharu <dani.kaharu17@gmail.com>
2022-08-11 21:37:47 +0800

    remove font style in summernote

--- resources/views/admin/articles/include/form.blade.php
+++ resources/views/admin/articles/include/form.blade.php
@@ -186,6 +186,19 @@
                 ['insert', ['link', 'picture', 'video']],
                 ['view', ['help']],
             ],
+            callbacks: {
+                onPaste: function(e) {
+                    var bufferText = ((e.originalEvent || e).clipboardData || window.clipboardData).getData(
+                        'Text');
+
+                    e.preventDefault();
+
+                    // Firefox fix
+                    setTimeout(function() {
+                        document.execCommand('insertText', false, bufferText);
+                    }, 10);
+                }
+            }
         });
 
         $('.note-editable').css('font-size', '1.125rem');
