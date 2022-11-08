0035ccb5d58b0a5787d16a1f805f05c31f8bb5ab
danikaharu <dani.kaharu17@gmail.com>
2022-11-08 12:14:58 +0800

    change copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,21 +1,16 @@
         <footer>
             <div class="footer clearfix mb-0 text-muted">
                 <div class="float-start">
-                    <p>{{ date('Y') }} &copy; Generator by
-                        <a href="https://github.com/Zzzul" target="_blank">M. Zulfahmi</a>
-                    </p>
-                </div>
-                <div class="float-end">
-                    <p>Mazer Admin by
-                        <a href="http://ahmadsaugi.com" target="_blank">A. Saugi</a>
+                    <p>{{ date('Y') }} &copy;
+                        <a href="https://berita.bonebolangokab.go.id" target="_blank">Berita Bone Bolango</a>
                     </p>
                 </div>
             </div>
         </footer>
-    </div>
+        </div>
 
-    <script src="{{ asset('mazer') }}/js/app.js"></script>
-    @stack('js')
-</body>
+        <script src="{{ asset('mazer') }}/js/app.js"></script>
+        @stack('js')
+        </body>
 
-</html>
+        </html>
