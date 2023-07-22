9b1f42b8aa46000e077b450bb8ed84bbc79c8489
danikaharu <dani.kaharu17@gmail.com>
2023-07-22 11:46:05 +0800

    change security header

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -22,12 +22,8 @@ class SecurityHeaders
             $response->headers->set('X-XSS-Protection', '1; mode=block');
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
             $response->headers->set('Expect-CT', 'enforce, max-age=30');
-            $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com cdn.datatables.net cdn.jsdelivr.net code.jquery.com www.google-analytics.com  www.googletagmanager.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' cdn.datatables.net cdnjs.cloudflare.com cdn.jsdelivr.net fonts.googleapis.com 'unsafe-inline'; img-src 'self' * data:; font-src 'self' fonts.gstatic.com cdnjs.cloudflare.com data: ; connect-src 'self' www.google-analytics.com ; media-src 'self'; frame-src 'self' www.google.com; object-src 'none'; base-uri 'self'; report-uri ");
+            $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' https://cdnjs.cloudflare.com ajax.googleapis.com cdn.datatables.net cdn.jsdelivr.net code.jquery.com www.google-analytics.com  www.googletagmanager.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' cdn.datatables.net cdnjs.cloudflare.com cdn.jsdelivr.net fonts.googleapis.com 'unsafe-inline'; img-src 'self' * data:; font-src 'self' fonts.gstatic.com cdnjs.cloudflare.com data: ; connect-src 'self' www.google-analytics.com ; media-src 'self'; frame-src 'self' www.google.com; object-src 'none'; base-uri 'self'; report-uri ");
             $response->headers->set('Permissions-Policy', 'autoplay=(self), camera=(), encrypted-media=(self), fullscreen=(), geolocation=(self), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=(self), usb=()');
-            $response->headers->set('Access-Control-Allow-Origin', '*');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token');
-
             $this->removeUnwantedHeaders($this->unwantedHeaders);
         }
 
